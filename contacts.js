9f55655d7c71d686470910e549a48b09dc50f00e
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-10-04 16:11:44 +0300

    add try/catch

--- contacts.js
+++ contacts.js
@@ -7,25 +7,40 @@ const contactsPath = path.join(__dirname, "/db/contacts.json");
 const updateContacts= async (contacts)=> await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 const listContacts=async ()=> {
-    const data = await fs.readFile(contactsPath)
-    return JSON.parse(data);
+    try {
+        const data = await fs.readFile(contactsPath)
+        return JSON.parse(data);
+        } catch (error) {
+        error.message = "There are some problems with response";
+        throw error;
+    }; 
 };
 
 const getContactById= async (contactId) => {
-    const contacts = await listContacts();
+    try {
+        const contacts = await listContacts();
     const res = contacts.find(item => item.id === contactId);
-    return res || null;
+        return res || null;
+        } catch (error) {
+        error.message = `Can't find contact with id ${contactId}`;
+        throw error;
+    }; 
 };
 
 const removeContact= async (contactId) =>{
-    const contacts = await listContacts();
+    try {
+        const contacts = await listContacts();
     const index = contacts.findIndex(item => item.id === contactId);
     if (index === -1) {
         return null;
     };
     const [res]=contacts.splice(index, 1);
     await updateContacts(contacts);
-    return res;
+        return res;
+         } catch (error) {
+        error.message = "Can't remove contact";
+        throw error;
+    };  
 };
 
 const addContact = async ({ name, email, phone }) => {
@@ -41,7 +56,7 @@ const addContact = async ({ name, email, phone }) => {
         await updateContacts(newContactsArr);
     return newContact;
     } catch (error) {
-        error.message = "Can't add book";
+        error.message = "Can't add contact";
         throw error;
     };  
 };
